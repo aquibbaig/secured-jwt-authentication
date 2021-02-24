4e53d1db7fdb2d7843ae72e02a70584436a2d41a
Aquib Baig <aquibbaig97@gmail.com>
2021-02-24 13:03:51 -0500

    login API in the backend

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,12 +1,38 @@
-import { hash } from 'bcryptjs';
-import {Resolver, Query, Mutation, Arg} from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import {Resolver, Query, Mutation, Arg, ObjectType, Field} from 'type-graphql';
 import { User } from './entity/User';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 
 @Resolver()
 export class UserResolver  {
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({email});
+
+    if (!user) {
+      throw new Error('could not find user with that email');
+    }
+    const valid = await compare(password, user.password);
+    if (!valid) {
+      throw new Error("Bad login")
+    }
+    return {
+      accessToken: sign({ userId: user.id}, "my_secret", {expiresIn: "15m"}),
+    }
   }
 
   @Mutation(() => Boolean)
