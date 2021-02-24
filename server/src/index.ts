70c9e7bd708ea7d354f165456cd53d3e2ba83f41
Aquib Baig <aquibbaig97@gmail.com>
2021-02-24 11:13:32 -0500

    add resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,22 +3,17 @@ import "reflect-metadata";
 import express from 'express';
 // import {User} from "./entity/User";
 import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers";
 
 (async () => {
   const app = express();
   app.get('/', (_, res) => res.send('Hello world'));
 
   const apolloServer = new ApolloServer({
-    typeDefs: `
-        type Query {
-            hello: String!
-        }
-    `,
-    resolvers: {
-        Query: {
-            hello: () => "hello world"
-        }
-    }
+    schema: await buildSchema({
+        resolvers: [UserResolver]
+    })
   });
 
   apolloServer.applyMiddleware({ app })
